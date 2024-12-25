2cebde3b36667dab673327e2cc95d23d31772bab
suryagupta1208 <suryaprakashgupta@makewebbetter.com>
2024-12-25 14:56:39 +0530

    Second commit to my Integrating With HubSpot I: Foundations practicum repository

--- index.js
+++ index.js
@@ -1,3 +1,4 @@
+require('dotenv').config();
 const express = require('express');
 const axios = require('axios');
 const app = express();
@@ -8,19 +9,59 @@ app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
 
 // * Please DO NOT INCLUDE the private app access token in your repo. Don't do this practicum in your normal account.
-const PRIVATE_APP_ACCESS = '';
+const PRIVATE_APP_ACCESS = process.env.PRIVATE_APP_ACCESS_TOKEN;
 
 // TODO: ROUTE 1 - Create a new app.get route for the homepage to call your custom object data. Pass this data along to the front-end and create a new pug template in the views folder.
 
 // * Code for Route 1 goes here
+app.get('/',(req, res)=>{
+    try {
+        const students = 'https://api.hubspot.com/crm/v3/objects/students?limit=100&properties=id,name,email';
+        const headers = {
+            Authorization: `Bearer ${PRIVATE_APP_ACCESS}`,
+            'Content-Type': 'application/json'
+        }
+        axios.get(students, { headers }).then(resp => {
+            const data = resp.data.results;    
+            res.render('homepage', { data });
+        })
+    } catch (error) {
+        console.log(error);
+    }
+
+})
 
 // TODO: ROUTE 2 - Create a new app.get route for the form to create or update new custom object data. Send this data along in the next route.
 
 // * Code for Route 2 goes here
+app.get('/update-cobj', (req, res) => {
+    res.render('updates', { title: 'Update Custom Object Form | Integrating With HubSpot I Practicum',  });
+})
 
 // TODO: ROUTE 3 - Create a new app.post route for the custom objects form to create or update your custom object data. Once executed, redirect the user to the homepage.
 
 // * Code for Route 3 goes here
+app.post('/update-cobj', (req, res) => {
+    const {id, name, email} = req.body
+    try {
+        const student = {
+            properties: {
+                "id": id,
+                "name": name,
+                "email": email
+            }
+        }
+        const createStudent = `https://api.hubspot.com/crm/v3/objects/students`;
+        const headers = {
+            Authorization: `Bearer ${PRIVATE_APP_ACCESS}`,
+            'Content-Type': 'application/json'
+        };
+        axios.post(createStudent, student, { headers });
+        res.redirect('/');
+    } catch (error) {
+        console.log(error);
+    }
+})
 
 /** 
 * * This is sample code to give you a reference for how you should structure your calls. 
